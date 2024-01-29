7313bb0d21cbfd07dae45adb9eaf782da7c49380
Fikri Nur <fikriiinur07@gmail.com>
2024-01-29 21:31:33 +0700

    migration + seeder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Analisis Sentimen</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Analisis Sentimen
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -30,7 +30,24 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Crawling Data</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('datasets') }}">Dataset</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Preprocessing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Naive Bayes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">SVM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Evaluasi</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -56,8 +73,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
