e03087c21b9e0063c556c8e104c29500e4c6c317
fikri-nur <fikriiinur07@gmail.com>
2024-04-30 05:15:17 +0700

    Update DataTable initialization and styling in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 import './bootstrap';
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+// import DataTable from 'datatables.net-dt';
+import DataTable from 'datatables.net-bs5';
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+
+// Inisialisasi DataTables
+$(document).ready(function() {
+    $('#dataset-table').DataTable({
+        autoWidth: false, // Menghentikan DataTables untuk mengatur lebar kolom secara otomatis
+        
+
+    });
+});
+
+
+$(document).ready(function() {
+    $('#preprocessing-table').DataTable();
+});
